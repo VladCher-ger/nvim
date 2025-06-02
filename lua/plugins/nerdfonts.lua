638da9b950589971ef87d5c3cbd08c5013a7b8c8
Vlad <w.cherniak@hotmail.de>
2025-06-02 12:29:09 +0200

    lazy runnning

---
+++ lua/plugins/nerdfonts.lua
@@ -0,0 +1,12 @@
+return{
+
+    {
+        'glepnir/nerdicons.nvim', 
+        cmd = 'NerdIcons', 
+    
+        config = function() 
+            require('nerdicons').setup({}) 
+        end
+    }
+
+}
